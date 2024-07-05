dff3c5e0b607e17f8bacf4769d42a10c5a65b06e
Mahyar Mirrashed <mahyar.mirrashed@jcatechnologies.com>
2024-07-05 13:39:45 -0500

    style: add catppuccin theme

--- lua/community.lua
+++ lua/community.lua
@@ -7,5 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }
