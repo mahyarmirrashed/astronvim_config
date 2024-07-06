9d8437161d035990052d7821609e4f6af6bd176e
Mahyar Mirrashed <mahyar.mirrashed@jcatechnologies.com>
2024-07-06 09:00:15 -0500

    feat: add go community pack

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.colorscheme.catppuccin" },
