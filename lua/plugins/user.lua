a637478fdf2ce5798638ddbe2979711f59247f1d
Mahyar Mirrashed <mahyar.mirrashed@jcatechnologies.com>
2024-07-10 12:15:58 -0500

    fix: enable lsp signature

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -28,7 +28,6 @@ return {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
     config = function() require("lsp_signature").setup() end,
-    enabled = false,
   },
 
   -- == Examples of Overriding Plugins ==
