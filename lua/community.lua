03736fb5bbb66d9120dad72c7fe9520f3078dc42
Mahyar Mirrashed <mahyar.mirrashed@jcatechnologies.com>
2024-09-17 12:16:11 -0700

    feat: add rust pack

--- lua/community.lua
+++ lua/community.lua
@@ -12,6 +12,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.terraform" },
   { import = "astrocommunity.pack.toml" },
